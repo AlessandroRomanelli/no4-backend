0f6a3342be54ef8adfab77ee6874382471cf60da
Alessandro Romanelli <aleromrod@gmail.com>
2020-04-07 02:27:26 +0200

    Added order to slots

--- lists/models/Slot.js
+++ lists/models/Slot.js
@@ -1,12 +1,13 @@
 const access = require("../../access");
-const { Relationship, Checkbox } = require("@keystonejs/fields");
+const { Relationship, Checkbox, Integer } = require("@keystonejs/fields");
 
 const SlotList = {
     fields: {
         role: { type: Relationship, ref: "Role", required: true },
         user: { type: Relationship, ref: "User.slot" },
         isOpen: { type: Checkbox, defaultValue: true },
-        group: { type: Relationship, ref: "Group.slots"}
+        group: { type: Relationship, ref: "Group.slots"},
+        order: { type: Integer }
     },
     // List-level access controls
     access: {
